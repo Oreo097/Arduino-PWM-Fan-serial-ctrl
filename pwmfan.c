9fb4a8deb8200f02cfa9fdf8d31733745a3374fc
Oreo097 <48041874+Oreo097@users.noreply.github.com>
2019-04-13 09:16:52 +0800

    Add files via upload

--- pwmfan.c
+++ pwmfan.c
@@ -8,17 +8,35 @@
 #include <errno.h>
 
 
+
+
+#define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
+
+
+
+
+
 char info[5]={0};
+int temp();
+int fd;
+char writeinfo;
+char dress="/dev/ttyUSB0";
+
+
+
+
 
 int init();
 int gettemp();
 int calu();
 int putinfo();
+int checkconf();
 
 
 
 int main()
 {
+   // checkconf();
     printf("start");
     init();
     printf("done");
@@ -34,8 +52,8 @@ int main()
 
 int init ()
 {
-    int fd;
-    fd=open("/dev/ttyUSB0",O_RDWR | O_NOCTTY | O_NONBLOCK);
+    
+    fd=open(dress,O_RDWR | O_NOCTTY | O_NONBLOCK);
     if(fd==-1)
     {
         printf("error");
@@ -67,15 +85,100 @@ int init ()
          read1=read(fd,rbuff,1);
     while(rbuff!='s');
 }
+
+
+
+
+
 int gettemp()
 {
-
+int file;
+double temp=0;
+char buf[MAX_SIZE];
+file=open(TEMP_PATH,O_RDONLY);
+if(file<0)
+{
+    printf("fail to open temp file\n");
+    return -1;
 }
+if(read(file,buf,MAX_SIZE)<0)
+{
+    printf("fail to read\n");
+    return -1;
+}
+temp=atoi(buf)\1000.0;
+}
+
+
+
+
+
+
+
 int calu()
 {
-
+    int oupt;
+    char status,ctemp[4];
+if(temp<20)
+{
+status='f';
 }
+else if(temp<25)
+{
+    status='e';
+}
+else if(tem<30)
+{
+    status='d';
+}
+else if (tem<35)
+{
+    status='c';
+}
+else if (tem<40)
+{
+    status='b';
+}
+else if (temp>=40)
+{
+    status='a';
+}
+info[4]=status;
+itoa(temp,ctemp,10);
+info[1]='0';
+info[2]=ctemp[0];
+info[3]=ctemp[1];
+info[0]='t';
+}
+
+
+
+
 int putinfo()
 {
+    int write;
+    char wbuff[5];
+    for(int i=0;i<=5;i++)
+    {
+        wbff[i]=info[i];
+    }
+    write=write(fd,wbuff,10);
+}
 
+
+
+
+
+
+
+int checkconf()
+{
+    FILE *fpr;
+    fpr=fopen("pwmfanconfig.conf","r");
+    if(fpr==NULL)
+    {
+        printf("file not found,start to creat config file\n");
+        fpr=fopen("pwmfanconfig.conf","w+");
+        fputc
+    }
 }
